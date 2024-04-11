8fb74c3464ec70f77a7642f189217334826710e2
cmshie05 <collinshields35@gmail.com>
2024-04-11 15:12:07 -0400

    logo update

--- tobacco-ui/src/components/Navbar.js
+++ tobacco-ui/src/components/Navbar.js
@@ -2,14 +2,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../Styling/NavBar.css"; // Import the CSS file
+import logo from "../Images/PHW_hor.jpg";
 
 const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container">
-        <Link className="navbar-brand" to="/">
-          Louisville Metro Public Helath and Wellness
-        </Link>
+        <img
+          className="logo-container"
+          src={logo}
+          alt="the logo for department of public health and wellness"
+        />
+
         <ul className="navbar-nav ml-auto">
           <li className="nav-item">
             <Link className="nav-link" to="/map">
